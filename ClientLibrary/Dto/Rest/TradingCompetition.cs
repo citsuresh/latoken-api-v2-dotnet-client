32d448d5569ba7c233da8d21524c7636e381444e
Suresh Kumar Veluswamy <sveluswa@itron.com>
2023-12-13 14:44:52 +0530

    Updated Json structure for Trading Competition api result

--- ClientLibrary/Dto/Rest/TradingCompetition.cs
+++ ClientLibrary/Dto/Rest/TradingCompetition.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 namespace Latoken.Api.Client.Library
 {
     public class TradingCompetition
@@ -21,16 +22,95 @@ namespace Latoken.Api.Client.Library
         [JsonProperty(PropertyName = "currency")]
         public string CurrencyId { get; set; }
 
-        [JsonProperty(PropertyName = "budget")]
-        public string Budget { get; set; }
-
-        [JsonProperty(PropertyName = "budgetSplit")]
-        public string BudgetSplit { get; set; }
-
         [JsonProperty(PropertyName = "target")]
         public string Target { get; set; }
 
+        [JsonProperty(PropertyName = "minTarget")]
+        public decimal MinTarget { get; set; }
+
         [JsonProperty(PropertyName = "winnersLimit")]
         public int WinnersLimit { get; set; }
+
+        [JsonProperty(PropertyName = "tag")]
+        public string Tag { get; set; }
+        public TopWinners TopWinners { get; set; }
+        public List<SplitBudget> SplitBudgets { get; set; }
+        public LuckyWinners LuckyWinners { get; set; }
+        public List<PlaceReward> PlaceRewards { get; set; }
+        public OthersReward OthersReward { get; set; }
     }
+
+    public class TopWinners
+    {
+        [JsonProperty("rewards")]
+        public List<string> rewards { get; set; }
+    }
+
+    public class SplitBudget
+    {
+        [JsonProperty("budget")]
+        public string budget { get; set; }
+
+        [JsonProperty("split")]
+        public string split { get; set; }
+
+        [JsonProperty("limit")]
+        public int limit { get; set; }
+
+        [JsonProperty("step")]
+        public int step { get; set; }
+    }
+
+    public class LuckyWinners
+    {
+        [JsonProperty("limit")]
+        public int limit { get; set; }
+
+        [JsonProperty("reward")]
+        public string reward { get; set; }
+
+        public override string ToString()
+        {
+            return limit.ToString() + " -> " + reward;
+        }
+    }
+
+
+    public class PlaceReward
+    {
+        [JsonProperty("from")]
+        public int from { get; set; }
+
+        [JsonProperty("to")]
+        public int to { get; set; }
+
+        [JsonProperty("rewardValue")]
+        public string rewardValue { get; set; }
+
+        public override string ToString()
+        {
+            if (from == to)
+            {
+                return from.ToString() + "->" + rewardValue;
+            }
+            else
+            {
+                return "(" + from.ToString() + "-" + to.ToString() +")" + "->" + rewardValue;
+            }
+        }
+    }
+
+    public class OthersReward
+    {
+        [JsonProperty("from")]
+        public int from { get; set; }
+
+        [JsonProperty("to")]
+        public int to { get; set; }
+
+        [JsonProperty("rewardValue")]
+        public object rewardValue { get; set; }
+    }
+
 }
+
